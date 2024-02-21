eb1c07d0eec6611df2593b3f2f6130e300f3bc85
Craig Carnell <craig_carnell@hotmail.com>
2024-02-21 19:06:25 +0000

    Add link to old cores

--- config.def.h
+++ config.def.h
@@ -1684,6 +1684,8 @@
 
 #if defined(HAKCHI)
 #define DEFAULT_BUILDBOT_SERVER_URL "http://hakchicloud.com/Libretro_Cores/"
+#elif defined(WEBOS)
+#define DEFAULT_BUILDBOT_SERVER_URL "https://dropbox.inf.re/retroarch-webos/cores/"
 #elif defined(ANDROID)
 #if defined(ANDROID_ARM_V7)
 #define DEFAULT_BUILDBOT_SERVER_URL "http://buildbot.libretro.com/nightly/android/latest/armeabi-v7a/"
